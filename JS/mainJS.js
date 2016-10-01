632c60245d3aaf86d0357e56b836fceb78dc17e5
spacewei <693906369@qq.com>
2016-10-01 19:12:13 +0800

    orgin
    20161001

---
+++ JS/mainJS.js
@@ -0,0 +1,12 @@
+/**
+ * Created by SPACEY on 2016/9/28.
+ */
+//function test(){
+//    $(".header-shop").innerHTML="呵呵";
+//    $(".header-shop").hide();
+//}
+
+$(document).ready(function(){
+    $
+
+});
